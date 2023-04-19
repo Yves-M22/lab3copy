6b067712ef7a47b283a8fbef468cbb52fd732708
Yves-M22 <sebastianYSM12@gmail.com>
2023-04-19 16:33:33 -0700

    Lab3arrays

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,24 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceWithArrayLength2() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 2, 1 }, input1);
+  }
+
+  @Test
+  public void testReverseInPlaceWithArrayLength3() {
+    int[] input1 = {3, 2, 1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1, 2, 3 }, input1);
+  }
+
+  @Test
+  public void testReversedWithArrayLength3() {
+    int[] input1 = {3, 6, 9};
+    assertArrayEquals(new int[]{9, 6, 3}, ArrayExamples.reversed(input1));
+  }
 }
